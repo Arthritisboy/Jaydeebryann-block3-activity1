6e42f44aa64a8cc02ff07e5baf6e41a8baf3c7f4
Renek <jaes.ang.up@phinmaed.com>
2023-12-06 16:23:07 +0800

    MP3 Music Player Quiz
    Play Button function added.
    Can play one song.
    Button pressed state fixed.
    
    To be fixed:
    Functions for Previous Song, Next Song and other files that are needed to be added.

--- app/src/main/java/com/ang/jaydeebryann/block3/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/ang/jaydeebryann/block3/p1/quiz/MainActivity.kt
@@ -3,13 +3,26 @@ package com.ang.jaydeebryann.block3.p1.quiz
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn_pause_play_song: ImageButton
 
+        val mediaPlayer: MediaPlayer = MediaPlayer.create(this, R.raw.murders)
+
+        btn_pause_play_song = findViewById(R.id.btn_pause_play_song)
+
+        btn_pause_play_song.setOnClickListener {
+            if(!mediaPlayer.isPlaying) {
+                mediaPlayer.start()
+                btn_pause_play_song.setImageResource(R.drawable.btn_pause_play_state)
+            } else {
+                mediaPlayer.pause()
+                btn_pause_play_song.setImageResource(R.drawable.btn_pause_state)
+            }
+        }
     }
 }
