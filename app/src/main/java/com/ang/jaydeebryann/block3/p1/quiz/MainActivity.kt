ff1ef12a640ccfe1c32dbc9293c7e03f77ca8749
Renek <jaes.ang.up@phinmaed.com>
2023-12-06 12:28:53 +0800

    MP3 Music Player Quiz
    Vector Asset Implementation complete.
    Layout with Background complete.
    Functions not yet implemented.
    2 mp3 files added.
    
    To be fixed:
    Functions and other files that are needed to be added.

---
+++ app/src/main/java/com/ang/jaydeebryann/block3/p1/quiz/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ang.jaydeebryann.block3.p1.quiz
+
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
